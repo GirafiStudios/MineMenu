c7847753f159f62a87dd87346387ef11ccbb09f2
dmillerw <dmillerw@gmail.com>
2014-07-29 16:06:24 -0700

    Hack to fix activatable sigil usage

--- src/main/java/dmillerw/menu/data/click/ItemClickAction.java
+++ src/main/java/dmillerw/menu/data/click/ItemClickAction.java
@@ -30,7 +30,22 @@ public class ItemClickAction implements IClickAction {
             ItemStack stack = player.inventory.getStackInSlot(i);
 
             if (stack != null && this.item.isItemEqual(stack)) {
-                stack.useItemRightClick(player.worldObj, player);
+                boolean client = true;
+
+                // Basic hack for allowing activatable sigils to be used without issue
+                // DAMN YOU WayOfTime >:(
+                Class<?> bloodMagic = null;
+                try {
+                    bloodMagic = Class.forName("WayofTime.alchemicalWizardry.common.items.EnergyItems");
+                } catch (ClassNotFoundException e) {}
+                if (bloodMagic != null && bloodMagic.isAssignableFrom(stack.getItem().getClass())) {
+                    client = false;
+                }
+
+                if (client) {
+                    stack.useItemRightClick(player.worldObj, player);
+                }
+
                 PacketHandler.INSTANCE.sendToServer(new PacketUseItem(i));
             }
         }
