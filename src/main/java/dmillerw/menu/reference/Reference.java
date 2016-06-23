662831adcf3f70e1247291637fe793b3d87aaf94
GirafiStudios <girafi0@gmail.com>
2016-06-23 10:39:59 +0200

    Ported to 1.10

--- src/main/java/dmillerw/menu/reference/Reference.java
+++ src/main/java/dmillerw/menu/reference/Reference.java
@@ -6,6 +6,6 @@ public class Reference {
     public static final String MOD_VERSION = "%MOD_VERSION%";
     public static final String CLIENT_PROXY_CLASS = "dmillerw.menu.proxy.ClientProxy";
     public static final String SERVER_PROXY_ClASS = "dmillerw.menu.proxy.CommonProxy";
-    public static final String DEPENDENCIES = "required-after:Forge@[12.16.1.1887,)";
+    public static final String DEPENDENCIES = "required-after:Forge@[12.18,)";
     public static final String GUI_FACTORY_CLASS = "dmillerw.menu.gui.config.MineMenuGuiFactory";
 }
