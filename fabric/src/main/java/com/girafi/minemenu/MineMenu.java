befa0a50e9c31d3aa05f643a1f5485309df0c5ea
GirafiStudios <faj10@msn.com>
2023-12-09 06:11:07 +0100

    Got Fabric version fully working

--- fabric/src/main/java/com/girafi/minemenu/MineMenu.java
+++ fabric/src/main/java/com/girafi/minemenu/MineMenu.java
@@ -2,13 +2,17 @@ package com.girafi.minemenu;
 
 import com.girafi.minemenu.data.menu.RadialMenu;
 import com.girafi.minemenu.gui.RadialMenuScreen;
+import com.girafi.minemenu.handler.ClientTickHelper;
 import com.girafi.minemenu.handler.KeyboardHandlerHelper;
 import com.girafi.minemenu.util.Config;
 import fuzs.forgeconfigapiport.api.config.v3.ForgeConfigRegistry;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
 import net.neoforged.fml.config.ModConfig;
 
 public class MineMenu implements ModInitializer {
@@ -28,6 +32,15 @@ public class MineMenu implements ModInitializer {
                 }
 
             KeyboardHandlerHelper.onClientTick();
+        });
+
+        HudRenderCallback.EVENT.register((guiGraphics, delta) -> {
+            Minecraft mc = Minecraft.getInstance();
+            if (mc.level != null && !mc.options.hideGui && !mc.isPaused() && RadialMenuScreen.active) {
+                ClientTickHelper.renderButtonBackgrounds();
+                ClientTickHelper.renderItems(guiGraphics);
+                ClientTickHelper.renderText(guiGraphics);
+            }
         });
     }
 }
