857dbd688cf791cc54c7a11952be961cbf5d2721
GirafiStudios <faj10@msn.com>
2024-06-16 17:31:58 +0200

    Fixed crash when creating a mine menu item on a server, without having done so in singleplayer first

--- neoforge/src/main/java/com/girafi/minemenu/MineMenu.java
+++ neoforge/src/main/java/com/girafi/minemenu/MineMenu.java
@@ -7,16 +7,18 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.loading.FMLPaths;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.level.LevelEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
 @Mod(Constants.MOD_ID)
 public class MineMenu {
 
     public MineMenu(ModContainer modContainer, IEventBus modBus) {
-        NeoForge.EVENT_BUS.addListener(EventPriority.LOWEST, this::loadMenuLoader);
+        NeoForge.EVENT_BUS.addListener(this::setupMenuLoader);
         modBus.addListener(this::registerKeybind);
 
         MineMenuCommon.registerPackets();
@@ -24,9 +26,9 @@ public class MineMenu {
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.spec);
     }
 
-    public void loadMenuLoader(ServerStartingEvent event) {
+    public void setupMenuLoader(LevelEvent.Load event) {
         MineMenuCommon.loadMenuJson(FMLPaths.GAMEDIR.get().toFile());
-        MineMenuCommon.setupMenuLoader(event.getServer().registryAccess());
+        MineMenuCommon.setupMenuLoader(event.getLevel().registryAccess());
     }
 
     public void registerKeybind(RegisterKeyMappingsEvent event) {
